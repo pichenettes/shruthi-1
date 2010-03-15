d8a97a88b3f47dfa36d378a8825aabae810d03eb
pichenettes <ol.gillet@gmail.com>
2010-03-15 02:32:08 +0100

    Added missing file (midi out filter)

---
+++ hardware/shruthi/midi_out_filter.h
@@ -0,0 +1,95 @@
+// Copyright 2010 Olivier Gillet.
+//
+// Author: Olivier Gillet (ol.gillet@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+//
+// This class is responsible for collecting from various sources messages that
+// might be potentially translated into output on the MIDI OUT.
+
+#ifndef HARDWARE_SHRUTHI_MIDI_OUT_FILTER_H_
+#define HARDWARE_SHRUTHI_MIDI_OUT_FILTER_H_
+
+#include "hardware/base/base.h"
+#include "hardware/hal/ring_buffer.h"
+#include "hardware/shruthi/patch.h"
+#include "hardware/shruthi/shruthi.h"
+
+namespace hardware_shruthi {
+
+class MidiOutFilter {
+ public:
+  enum {
+    buffer_size = 128,
+    data_size = 8,
+  };
+  typedef hardware_hal::Buffer<MidiOutFilter> OutputBuffer;
+  typedef hardware_hal::DataTypeForSize<data_size>::Type Value;
+
+  MidiOutFilter() { }
+  
+  static void UpdateParameters(const Patch& patch) {
+    channel_ = patch.sys_midi_channel == 0 ? 0 : patch.sys_midi_channel - 1;
+    mode_ = patch.sys_midi_out_mode;
+  }
+  
+  static void KnobTweaked(uint8_t id, uint16_t value) {
+    if (mode_ == MIDI_OUT_CONTROLLER) {
+      OutputBuffer::Overwrite(0xb0 | channel_);
+      OutputBuffer::Overwrite(16 + id);
+      OutputBuffer::Overwrite(value >> 3);
+    }
+  }
+  
+  static void NoteKilled(uint8_t note) {
+    if (mode_ == MIDI_OUT_SEQUENCER) {
+      OutputBuffer::Overwrite(0x90 | channel_);
+      OutputBuffer::Overwrite(note);
+      OutputBuffer::Overwrite(0);
+    }
+  }
+
+  static void NoteTriggered(uint8_t note, uint8_t velocity) {
+    if (mode_ == MIDI_OUT_SEQUENCER) {
+      OutputBuffer::Overwrite(0x90 | channel_);
+      OutputBuffer::Overwrite(note);
+      OutputBuffer::Overwrite(velocity);
+    }
+  }
+  
+  static uint8_t readable() {
+    return OutputBuffer::readable();
+  }
+
+  static uint8_t ImmediateRead() {
+    return OutputBuffer::ImmediateRead();
+  }
+  
+  static void RawDataReceived(uint8_t data) {
+    if (mode_ == MIDI_OUT_SOFT_THRU) {
+      OutputBuffer::Overwrite(data);
+    }
+  }
+    
+  static uint8_t mode_;
+  static uint8_t channel_;
+
+  DISALLOW_COPY_AND_ASSIGN(MidiOutFilter);
+};
+
+extern MidiOutFilter midi_out_filter;
+
+}  // namespace hardware_shruthi
+
+#endif  // HARDWARE_SHRUTHI_MIDI_OUT_FILTER_H_
