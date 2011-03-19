5fce090c4672c586790e4970e7dd30bdade4d4c5
pichenettes <ol.gillet@gmail.com>
2011-03-19 18:22:52 +0100

    Added simulations for the SSM2164 SVF

---
+++ hardware/shruthi/hardware_design/simulations/ssm_2164_svf_transfer.py
@@ -0,0 +1,45 @@
+import pylab
+import numpy
+
+f = 20 * (2 ** numpy.linspace(0, 10.0, 1000))
+w = 2 * numpy.pi * f
+
+Rg = 33000.0
+Ri = 33000.0
+Rb = 22000.0
+R = 33000.0
+C = 220e-12
+
+
+def transfer(f_cv, q_cv, w, output):
+  s = 1j * w
+  
+  alpha = -1 / (R * C * s) * 10 ** (-1.5 * f_cv)
+  beta = Rb / R * 10 ** (-1.5 * q_cv)
+
+  g = Rg / Ri
+  q = 1 / (2 + g) * 1.0 / beta
+  wc = 1 / (R * C) * (10 ** (-1.5 * f_cv))
+
+  print g, q, wc
+  
+  lp = -g / (s ** 2 / wc ** 2 + s / w * 1 / q + 1)
+  bp = lp / alpha
+  hp = bp / alpha
+  
+  return [lp, bp, hp][output]
+
+if __name__ == '__main__':
+  main()
+  num_freqs = 5
+  num_q = 4
+
+  for mode in xrange(3):
+    for row, q_cv in enumerate(numpy.linspace(0, 2.0, num_q)):
+      pylab.subplot(num_q, 3, mode + row * 3 + 1)
+      t = numpy.zeros((num_freqs, f.shape[0]), dtype=numpy.complex)
+      for f_index, f_cv in enumerate(numpy.linspace(0.5, 2.0, num_freqs)):
+        t[f_index, :] = transfer(f_cv, q_cv, w, mode)
+      pylab.semilogx(f, 20 * numpy.log10(abs(t)).T)
+
+  pylab.show()
