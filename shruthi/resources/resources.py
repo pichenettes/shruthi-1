7c1da1fc394dbd96fb994e9a3f0080087a6df7b4
Emilie Gillet <ol.gillet@gmail.com>
2019-04-02 13:15:05 +0200

    Updated name

--- shruthi/resources/resources.py
+++ shruthi/resources/resources.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python2.5
 #
-# Copyright 2009 Olivier Gillet.
+# Copyright 2009 Emilie Gillet.
 #
-# Author: Olivier Gillet (ol.gillet@gmail.com)
+# Author: Emilie Gillet (emilie.o.gillet@gmail.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,9 +19,9 @@
 #
 # Master resources file.
 
-header = """// Copyright 2009 Olivier Gillet.
+header = """// Copyright 2009 Emilie Gillet.
 //
-// Author: Olivier Gillet (ol.gillet@gmail.com)
+// Author: Emilie Gillet (emilie.o.gillet@gmail.com)
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
